5f330d50209a331ad4ce7f5617dc5cbd54852388
tanvir-ahmed <ahmedtanvir444@gmail.com>
2022-10-11 11:51:34 +0600

    header added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <nav className='flex'>
-            <h3>Justify Content</h3>
+        <nav className='flex justify-between px-20 py-4 bg-sky-500'>
+            <Link to='/' className='font-bold text-2xl'><span className='bg-indigo-700 text-teal-100 p-2 rounded-l-lg'>Justify</span><span className='bg-black p-2 text-white rounded-r-lg'>Content</span></Link>
             <div>
-                <Link to ='/home'>Home</Link>
-                <Link to ='/statistics'>Statistics</Link>
-                <Link to ='/blog'>Blog</Link>
-                <Link to ='/about'>About</Link>
+                <Link className='ml-8 font-semibold text-yellow-50 hover:text-yellow-400' to ='/home'>Home</Link>
+                <Link className='ml-8 font-semibold text-yellow-50 hover:text-yellow-400' to ='/statistics'>Statistics</Link>
+                <Link className='ml-8 font-semibold text-yellow-50 hover:text-yellow-400' to ='/blog'>Blog</Link>
+                <Link className='ml-8 font-semibold text-yellow-50 hover:text-yellow-400' to ='/about'>About</Link>
             </div>
         </nav>
     );
